cbb75536db7e05a1b386e1131bbaf4d1be26b369
Kanagroo <68487042+kanagrou@users.noreply.github.com>
2022-04-13 20:10:29 -0400

    .gitignore

--- dist/event.d.ts
+++
@@ -1,7 +0,0 @@
-import { ClientEvents } from 'discord.js';
-export declare type TListener = (...args: any) => Promise<void>;
-export default interface Event {
-    event: keyof ClientEvents;
-    guild?: string;
-    listener: TListener;
-}
--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { default as KCommandManager } from './commandmanager';
-export { default as KEventManager } from './eventmanager';
-export { default as KCommand } from './command';
-export { default as KEvent } from './event';
-export { default as KComponents } from './components';
-export { default as KButton } from './button';
-export { default as KSelect } from './select';
